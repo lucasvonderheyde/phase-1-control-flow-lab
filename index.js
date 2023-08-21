09bd01af6d9a2499423be800f7dc8b405d264a3c
Lucas Von der Heyde <lucasvonderheyde@gmail.com>
2023-08-21 10:24:21 -0400

    finish lab

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400) {
+    return "This one is on me!";
+  }
+
+  else if (400 < ride && ride < 2000) {
+    return "That will be twenty bucks."
+  }
+
+  else if (2000 <= ride && ride < 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+
+  else if (2500 <= ride) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you."
+    case 'thanks for everything':
+      return "Bye."
+  }
+
 }
